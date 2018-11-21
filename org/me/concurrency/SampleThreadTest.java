c1a4bc14146c41087308bc90bfe88a638f366901
nagesh2086 <nagesh2086@gmail.com>
2018-11-21 19:27:40 +0530

    Test join method - SampleThreadTest

---
+++ org/me/concurrency/SampleThreadTest.java
@@ -0,0 +1,41 @@
+package org.me.concurrency;
+
+/**
+ * Test join method - When we invoke the join() method on a thread, the calling
+ * thread goes into a waiting state. It remains in a waiting state until the
+ * referenced thread terminates.
+ **/
+public class SampleThreadTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread t = new Thread(new SampleThread(100), "thread-1_");
+		t.start();
+		System.out.println(Thread.currentThread().getName() + " waiting because of join");
+		t.join();
+		System.out.println(Thread.currentThread().getName() + " back from join and done");
+	}
+
+}
+
+class SampleThread implements Runnable {
+	private int processingCount = 0;
+
+	SampleThread(int processingCount) {
+		this.processingCount = processingCount;
+	}
+
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + " started");
+		while (processingCount > 0) {
+			try {
+
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.println(Thread.currentThread().getName() + " interrupted");
+			}
+			processingCount--;
+		}
+		System.out.println(Thread.currentThread().getName() + " exited");
+	}
+}
