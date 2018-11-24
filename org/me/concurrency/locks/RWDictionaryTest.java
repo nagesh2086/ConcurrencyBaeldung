c111617e0ed6318ee05a3dfc0b6801efcb3e84f3
nagesh2086 <nagesh2086@gmail.com>
2018-11-24 10:29:20 +0530

    RWDictionaryTest modified

--- org/me/concurrency/locks/RWDictionaryTest.java
+++ org/me/concurrency/locks/RWDictionaryTest.java
@@ -12,25 +12,21 @@ public class RWDictionaryTest {
 		Data d7 = new Data("Nageshxxxxxx", 38);
 
 		RWDictionary rwDict = new RWDictionary();
-		//System.out.println(rwDict.allKeys());
-		//System.out.println(rwDict.toString());
-		//System.out.println(rwDict.get("Nageshxxxx"));
-		//System.out.println(rwDict.get("Nageshxxxxxxxx"));
-		//System.out.println(rwDict.get("Nageshxxx"));
-		System.out.println(rwDict.put("Nagesh", d1));
-		System.out.println(rwDict.put("Nageshx", d2));
-		System.out.println(rwDict.put("Nageshxx", d3));
 		System.out.println(rwDict.allKeys());
-		//System.out.println(rwDict.get("Nageshxx"));
-		System.out.println(rwDict.put("Nageshxxx", d4));
-		System.out.println(rwDict.put("Nageshxxxx", d5));
-		//System.out.println(rwDict.get("Nageshxxxx"));
-		System.out.println(rwDict.put("Nageshxxxxx", d6));
-		System.out.println(rwDict.put("Nageshxxxxxx", d7));
+		System.out.println(rwDict.get("Nageshxxx"));
+		rwDict.put("Nagesh", d1);
+		rwDict.put("Nageshx", d2);
+		rwDict.put("Nageshxx", d3);
 		System.out.println(rwDict.allKeys());
-		System.out.println(rwDict.put("Nageshxxxxx", d6));
-		System.out.println(rwDict.put("Nageshxxxxx", d6));
-		//rwDict.clear();
+		System.out.println(rwDict.get("Nageshxx"));
+		rwDict.put("Nageshxxx", d4);
+		rwDict.put("Nageshxxxx", d5);
+		System.out.println(rwDict.get("Nageshxxxx"));
+		rwDict.put("Nageshxxxxx", d6);
+		rwDict.put("Nageshxxxxxx", d7);
+		System.out.println(rwDict.allKeys());
+		System.out.println(rwDict.put("Nageshxxxxx", d7));
+		rwDict.clear();
 		System.out.println(rwDict.allKeys());
 	}
 
